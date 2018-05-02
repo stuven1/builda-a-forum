b4bd394c89e879f95de4ffce8884c773ad6cf77b
ader <adamsson.erika@gmail.com>
2018-05-02 15:49:56 +0200

    Q/A works backend

--- backend/server.js
+++ backend/server.js
@@ -18,29 +18,28 @@ mongoose.connection.once("open", () => console.log("Connected to mongodb"))
 
 const Question = mongoose.model("Question", {
   name: String,
+  headline:String,
+  content: String
 })
 
 app.get("/", (req, res) => {
   res.send("Admin API")
 })
 
-app.post("/admin", (req, res) => {
-  const admin = new Question(req.body)
+app.post("/faq", (req, res) => {
+  const question = new Question(req.body)
   console.log("test")
 
-  admin.save()
+  question.save()
     .then(() => { res.status(201).send("Question created") })
     .catch(err => { res.status(400).send(err) })
 })
 
-// app.get("/products", (req, res) => {
-//   Product.find().then(products => {
-//
-//     const obj = {products: products};
-//
-//     res.json(products)
-//   })
-// })
+app.get("/faq", (req, res) => {
+  Question.find().then(questions => {
+  res.json(questions)
+  })
+})
 
 
 app.listen(8080, () => {
