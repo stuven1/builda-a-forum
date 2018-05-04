e4cc4862ca300320e6d72f9a91fb73d5ec933887
stuven1 <moaheterjag@icloud.com>
2018-05-04 12:10:30 +0200

    added questions in admin

--- frontend/src/components/admin-view/index.js
+++ frontend/src/components/admin-view/index.js
@@ -1,11 +1,73 @@
 import React from "react"
 
 export default class AdminView extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+      headline: "",
+      content: "",
+      questions:[]
+    }
+  }
+
+  componentDidMount() {
+    this.getQuestions()
+  }
+  getQuestions = () => {
+    fetch("http://localhost:8080/faq", {
+      method: "GET"
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      console.log(json)
+      this.setState({
+        questions: json
+      })
+
+    })
+    .catch(err => {
+      // api down? request failed?
+      console.log("Error!", err)
+    })
+  }
+
+
+  handleName = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleHeadline = event => {
+    this.setState({
+      headline: event.target.value
+    })
+  }
+
+  handleContent = event => {
+    this.setState({
+      content: event.target.value
+    })
+  }
+
+  renderQuestions() {
+    return this.state.questions.map((question, index) => (
+      <div key={index} >
+        <div>{question.name}</div>
+        <div>{question.header}</div>
+        <div>{question.content}</div>
+    </div>)
+  )
+  }
 
   render() {
     return (
+
       <div>
-        Admin here
+        {this.renderQuestions()}
       </div>
     )
   }
